d4a4e85737d5da8aa5444c5d981176a5e7d29242
unknown <nemolaog@gmail.com>
2018-09-13 16:22:10 -0400

    almost

--- js/video.js
+++ js/video.js
@@ -5,64 +5,14 @@
 	var theExt = '.mp4';
 	
 	var buttonGroup = document.querySelectorAll(".vidLoader");
-	var volumeUp = document.querySelector("#volUp");
-	var volumeDown = document.querySelector("#volDown");
-	var muteVideo = document.querySelector("#volMute");
 	var pPlay = document.querySelector("input");
-	
-	
-	theControls.classList.toggle("hideMe");
-	theVideo.controls = false;
+	var video1=document.getElementById("videoPlay1");
 
-	function swapVideoSource(e) {
-	theVideo.src = "video/" + e.target.id + theExt; // build the path to the new video file
-	theVideo.load(); // load it
-	theVideo.play(); // play it
-	}
-
-
-	function vidLouder() {
-		if (theVideo.volume <= 1) {
-		theVideo.volume += .2;
-		}
-		//console.log(theVideo.volume);
-	}
-
-
-	function vidSofter() {
-		if (theVideo.volume >= 0) {
-		theVideo.volume -= .2;
-		}
-	}
-
-
-	function muteVid() {
-		theVideo.volume = 0;
-	}
-
-
-	function pausePlay() {
-		currentSource = theVideo.currentSrc; 
-		 theExt = currentSource.substr(currentSource.lastIndexOf("."));
-		 //console.log(theExt);
-		if (theVideo.paused) {
-			theVideo.play();
-			pPlay.value = "Pause";
-		} else {
-			theVideo.pause();
-			pPlay.value = "Play";
-		}
-	}
-
-
-	volumeUp.addEventListener("click", vidLouder, false);
-	volumeDown.addEventListener("click", vidSofter, false);
-	muteVideo.addEventListener("click", muteVid, false);
-	pPlay.addEventListener("click", pausePlay, false);
-	//theVideo.addEventListener('ended',showMessage,false);
-
-	for (i=0; i<buttonGroup.length; i++) {
-		buttonGroup[i].addEventListener("click", swapVideoSource, false);
-	}
-
+video1.onclick=function(){
+    if(video1.paused){
+        video1.play();
+    }else{
+        video1.pause();
+    }
+}
 
